b2e70b51ef62b28e0fc558ba970acd79c0d95e95
Caridad RoblesCarvajal <croblescarvajal@xpanxion.com>
2023-07-03 22:53:48 -0400

    Second commit:

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,1 +1,10 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/members")
+def member():
+    return{"members": ["Member1", "Member2", "Member3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
